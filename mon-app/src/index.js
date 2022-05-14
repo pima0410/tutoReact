fa63e8808540ddb180652cdc182a3e9655df404a
pierre <pierre.parra@gmail.com>
2022-05-14 10:09:46 +0200

    ajout index square

--- mon-app/src/index.js
+++ mon-app/src/index.js
@@ -6,7 +6,12 @@ class Square extends React.Component{
     
    
     render(props){
-        return (<button className="square" onClick={ this.props.onClick} /> )
+       // console.log(this.props.key);
+        return (
+        <button className="square" onClick={ this.props.onClick} >
+            {this.props.index}
+        </button> 
+        )
         //return(<div class="square">{this.props.value}</div>)
     }
 }
@@ -15,20 +20,27 @@ class Game extends React.Component{
    constructor(props){
        super(props);
        this.state={
-        orderLetter : "X"
+        orderLetter : ""
        
        }
      
+
    }
 
-   handleClick(e){
+   handleClick(e, index){
        let orderLetterTemp;
-    e.currentTarget.innerHTML=this.state.orderLetter;
-    this.state.orderLetter ==="X" ? orderLetterTemp="O"  : orderLetterTemp ="X";
-   console.log(this.state.orderLetter)
-   this.setState({
-       orderLetter :orderLetterTemp
-   })
+    
+       if(e.currentTarget.innerHTML===""){
+       // e.currentTarget.innerHTML=this.state.orderLetter;
+        this.state.orderLetter ==="X" ? orderLetterTemp="O"  : orderLetterTemp ="X";
+        this.setState({
+            orderLetter :orderLetterTemp
+        })
+       }
+    
+   
+   
+   
     //useState.orderLetter ==="X" ? this.setState.orderLetter="O"  : this.setState.orderLetter ="X";
 }
 
@@ -40,7 +52,8 @@ class Game extends React.Component{
             divCell= [];
            for(let j = 0; j < 3; j++){
             divCell.push(<Square value={this.state.orderLetter} 
-                            key={j}  
+                            key={i+""+j} 
+                            index={i+""+j} 
                             onClick={(e)=>this.handleClick(e)}
                             
                         />)  
